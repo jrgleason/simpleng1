b29c9447b32d234da3ec363c77b923d5b7640ecb
jrgleason <jackiegleason@gmail.com>
2017-04-23 00:42:45 -0400

    Updating for the question

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 (function(){
-  var module = angular.module("test", []);
+  var module = angular.module("test", ['ngRoute']);
   var TestController = function($scope){
     var _this = this;
   }
-  module.directive("jgHello", function(){
+  var RegularController = function(){
+  }
+  var SearchController = function($scope, $routeParams){
+    $scope.text = $routeParams.text;
+  }
+  module.directive("jgTest", function(){
     return {
       template: "<h1>Hello</h1>",
       controller: TestController,
       controllerAs: "tc"  
     }
   })
+  module.config(function($routeProvider, $locationProvider) {
+    $routeProvider.when('/', {
+      template: '<h1>Regular</h1>',
+      controller: RegularController,
+    })
+    .when('/search/:text', {
+      template: '<h1>{{text}}</h1>',
+      controller: SearchController
+    });
+  });
 })()
